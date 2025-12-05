3f907579269132730a684bd4f7c1cbf70ad89b41
Kiran Panchavati <kiranpanchavati9@gmail.com>
2025-12-05 13:18:44 +0530

    catalogue-redirectors.sh file updated

--- catalogue-redirectors.sh
+++ catalogue-redirectors.sh
@@ -2,15 +2,18 @@ echo Disable NodeJS Default Version
 
 echo
 dnf module disable nodejs -y  &>>/tmp/roboshop.log
+echo $?
 
 echo Disable NodeJS 20 Version
 
 dnf module enable nodejs:20 -y &>>/tmp/roboshop.log
+echo $?
 
 echo Install Node JS
 echo
 
 dnf install nodejs -y  &>>/tmp/roboshop.log
+echo $?
 
 echo Add Application User
 echo
@@ -20,30 +23,47 @@ useradd roboshop
 echo Copy SystemD Service File
 
 cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+echo $?
 
 echo Copy Mongod repo File
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-echo Create Application Directory
+echo Clean Old Application Directory
+rm -rf /app &>>/tmp/roboshop.log
+echo $?
 
+echo $?
+echo Create Application Directory
 mkdir /app &>>/tmp/roboshop.log
+echo $?
+
 
 echo Download Application Content
-
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>/tmp/roboshop.log
+echo $?
+
 cd /app
+echo $?
+
 unzip /tmp/catalogue.zip 
-
+echo $?
 
 cd /app
+echo $?
+
 npm install
+echo $?
 
 echo install mongodb
 
 dnf install mongodb-mongosh -y
+echo $?
+
 mongosh --host mongod-dev.kiranpanchavati.online </app/db/master-data.js &>>/tmp/roboshop.log
+echo $?
 
 systemctl daemon-reload
+echo $?
 
 systemctl enable catalogue
 systemctl restart catalogue
