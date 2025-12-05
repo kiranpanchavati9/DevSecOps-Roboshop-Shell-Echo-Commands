364b6a57da7411e4b200fd4a2127964d1402fab6
Kiran Panchavati <kiranpanchavati9@gmail.com>
2025-12-05 12:58:03 +0530

    catalogue-redirectors.sh file updated

--- catalogue-redirectors.sh
+++ catalogue-redirectors.sh
@@ -1,16 +1,16 @@
 echo Disable NodeJS Default Version
 
 echo
-dnf module disable nodejs -y  &>/tmp/roboshop.log
+dnf module disable nodejs -y  &>>/tmp/roboshop.log
 
 echo Disable NodeJS 20 Version
 
-dnf module enable nodejs:20 -y &>/tmp/roboshop.log
+dnf module enable nodejs:20 -y &>>/tmp/roboshop.log
 
 echo Install Node JS
 echo
 
-dnf install nodejs -y  &>/tmp/roboshop.log
+dnf install nodejs -y  &>>/tmp/roboshop.log
 
 echo Add Application User
 echo
@@ -19,18 +19,18 @@ useradd roboshop
 
 echo Copy SystemD Service File
 
-cp catalogue.service /etc/systemd/system/catalogue.service &>/tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 echo Copy Mongod repo File
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>/tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo Create Application Directory
 
-mkdir /app &>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
 
 echo Download Application Content
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>/tmp/roboshop.log
 cd /app
 unzip /tmp/catalogue.zip 
 
@@ -41,7 +41,7 @@ npm install
 echo install mongodb
 
 dnf install mongodb-mongosh -y
-mongosh --host mongod-dev.kiranpanchavati.online </app/db/master-data.js &>/tmp/roboshop.log
+mongosh --host mongod-dev.kiranpanchavati.online </app/db/master-data.js &>>/tmp/roboshop.log
 
 systemctl daemon-reload
 
